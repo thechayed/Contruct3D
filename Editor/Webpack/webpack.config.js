f619fa8db157c49d9d6f49476ffce54db05ffc4e
thechayed <thechayed@gmail.com>
2024-06-25 12:56:47 -0400

    Initial Commit with Chrome Plugin and the module based Editor Project including Webpack to build for the Plugin

---
+++ Editor/Webpack/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: '../Editor.js', // The entry point of your application
+  output: {
+    filename: 'C3D_Editor_Webpack.js', // The name of the output file
+    path: path.resolve(__dirname, '../../Chrome Plugin'), // The directory where the output will be placed
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Test for JavaScript files
+        exclude: /node_modules/, // Exclude node_modules from being bundled
+        use: {
+          loader: 'babel-loader', // Use Babel loader for transpiling ES6+ code
+        },
+      },
+    ],
+  },
+};
